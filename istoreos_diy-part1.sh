d9e43de593eb2bc7b5f3a21f93020bafc182c9f1
hza1128 <141570215+hza1128@users.noreply.github.com>
2025-04-27 08:48:43 +0800

    Update istoreos_diy-part1.sh

--- istoreos_diy-part1.sh
+++ istoreos_diy-part1.sh
@@ -21,4 +21,6 @@
 #echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 #echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 #echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kiddin9/kwrt-packages' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.defaul
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages' feeds.conf.default
+
